d518e78d42bb8a25188b4228d35810720e56f383
Aibek Bakirov <ssak.group@gmail.com>
2021-09-14 23:44:46 +0300

    Exercise is modified

--- week4/ex4.c
+++ week4/ex4.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-	int c = 0;
-	int n = fork();
-	system();
+int main(void) {
+	char c[1000];
+	char *str[100], *path="/home/comp/Operating-Systems/week4";
+	int i = 0;
+	fgets(c, 1000, stdin);
+	char *nstr = strtok(c, " ");
+	str[i ++] = path;
+	while (nstr != NULL) {
+		str[i ++] = nstr;
+		nstr = strtok(NULL, " ");
+	}
+	str[i] = NULL;
+	if (fork() == 0) {
+		execve(path, str, NULL);
+	}
 	return EXIT_SUCCESS;
 }
+
