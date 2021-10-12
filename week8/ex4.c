571aeab7cc687e6403dc7726f5a2f3d0f4d3384a
aibek99 <aibek.bakirov99@gmail.com>
2021-10-13 00:57:33 +0400

    Done

--- week8/ex4.c
+++ week8/ex4.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/resource.h>
 
 int main(void) {
     for (int i = 1; i <= 10; i ++) {
         int sz = 10 * 1024 * 1024;
         char *ar = malloc(sz);
         memset(ar, 0, sz);
+        struct rusage memory;
+        getrusage(RUSAGE_SELF, &memory);
+        printf("Memuse: %ld\n", memory.ru_maxrss);
         sleep(1);
     }
     return EXIT_SUCCESS;
